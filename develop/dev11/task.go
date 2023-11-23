2b25ad6238a651db1ef1f789c61cbb5a926c1b60
xandrozzz <evg.alexandrov95@gmail.com>
2023-11-23 08:49:12 +0300

    minor changes

--- develop/dev11/task.go
+++ develop/dev11/task.go
@@ -13,89 +13,107 @@ import (
 	"time"
 )
 
+// event - класс события календаря
 type event struct {
 	Date   time.Time `json:"date"`
-	UserId int       `json:"user_id"`
+	UserID int       `json:"user_id"`
 }
 
-func newEvent(date time.Time, userId int) *event {
+// newEvent - конструктор класса event
+func newEvent(date time.Time, userID int) *event {
 	return &event{
 		Date:   date,
-		UserId: userId,
+		UserID: userID,
 	}
 }
 
+// eventStorage - класс хранилища событий
 type eventStorage struct {
 	events []*event
 }
 
+// eventStorage - конструктор класса eventStorage
 func newEventStorage() *eventStorage {
 	return &eventStorage{
 		events: make([]*event, 0),
 	}
 }
 
+// addEvent - метод для добавления события в хранилище
 func (es *eventStorage) addEvent(newEvent *event) {
 	es.events = append(es.events, newEvent)
 }
 
-func (es *eventStorage) removeEvent(date time.Time, userId int) error {
+// removeEvent - метод для удаления события из хранилища
+func (es *eventStorage) removeEvent(date time.Time, userID int) error {
+	// поиск индекса удаляемого события
 	eventIndex := slices.IndexFunc(es.events, func(e *event) bool {
-		if e.Date == date && e.UserId == userId {
+		if e.Date == date && e.UserID == userID {
 			return true
-		} else {
-			return false
 		}
+		return false
 	})
+	// возврат ошибки, если событие не найдено
 	if eventIndex == -1 {
 		return errors.New("event not found")
 	}
-	slices.Delete(es.events, eventIndex, eventIndex+1)
+	slices.Delete(es.events, eventIndex, eventIndex+1) // удаление события
 	return nil
 }
 
-func (es *eventStorage) updateEvent(date time.Time, userId int, newDate time.Time) error {
+// updateEvent - метод для обновления события
+func (es *eventStorage) updateEvent(date time.Time, userID int, newDate time.Time) error {
+	// поиск индекса обновляемого события
 	eventIndex := slices.IndexFunc(es.events, func(e *event) bool {
-		if e.Date == date && e.UserId == userId {
+		if e.Date == date && e.UserID == userID {
 			return true
-		} else {
-			return false
 		}
+		return false
+
 	})
+	// возврат ошибки, если событие не найдено
 	if eventIndex == -1 {
 		return errors.New("event not found")
 	}
-	es.events[eventIndex].Date = newDate
+	es.events[eventIndex].Date = newDate // обновление события
 	return nil
 }
 
+// findEventByDay - метод поиска событий по дню
 func (es *eventStorage) findEventByDay(date time.Time) ([]*event, error) {
+	// поиск событий и добавление их в слайс
 	foundEvents := make([]*event, 0)
 	for _, storedEvent := range es.events {
 		if storedEvent.Date == date {
 			foundEvents = append(foundEvents, storedEvent)
 		}
 	}
+	// если слайс пустой, возврат ошибки
 	if len(foundEvents) == 0 {
 		return nil, errors.New("event not found")
 	}
 	return foundEvents, nil
 }
 
+// findEventByMonth - метод поиска событий по месяцу
 func (es *eventStorage) findEventByMonth(date time.Time) ([]*event, error) {
+	// поиск событий и добавление их в слайс
 	foundEvents := make([]*event, 0)
 	for _, storedEvent := range es.events {
 		if storedEvent.Date.Month() == date.Month() && storedEvent.Date.Year() == date.Year() {
 			foundEvents = append(foundEvents, storedEvent)
 		}
 	}
+	// если слайс пустой, возврат ошибки
 	if len(foundEvents) == 0 {
 		return nil, errors.New("event not found")
 	}
 	return foundEvents, nil
 }
 
+// findEventByWeek - метод поиска событий по неделе
 func (es *eventStorage) findEventByWeek(date time.Time) ([]*event, error) {
+	// поиск событий и добавление их в слайс
 	foundEvents := make([]*event, 0)
 	for _, storedEvent := range es.events {
 		_, storedWeek := storedEvent.Date.ISOWeek()
@@ -104,24 +122,30 @@ func (es *eventStorage) findEventByWeek(date time.Time) ([]*event, error) {
 			foundEvents = append(foundEvents, storedEvent)
 		}
 	}
+	// если слайс пустой, возврат ошибки
 	if len(foundEvents) == 0 {
 		return nil, errors.New("event not found")
 	}
 	return foundEvents, nil
 }
 
-var keyServerAddr = "serverAddr"
+// ключ от адреса сервера для контекста
+var keyServerAddr = []byte("serverAddr")
 
+// глобальное хранилище событий
 var serverEventStorage = newEventStorage()
 
+// класс ответа на неверный запрос
 type badResponse struct {
 	Error string `json:"error"`
 }
 
+// класс ответа на верный запрос
 type goodResponse struct {
 	Result string `json:"result"`
 }
 
+// createEvent - хэндлер функция для сервера, создающая новое событие
 func createEvent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -142,8 +166,8 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 
 	hasDate := r.Form.Has("date")
 	date := r.Form.Get("date")
-	hasUserId := r.Form.Has("user_id")
-	userId := r.Form.Get("user_id")
+	hasUserID := r.Form.Has("user_id")
+	userID := r.Form.Get("user_id")
 
 	convertedDate, err := time.Parse(time.DateOnly, date)
 	if err != nil {
@@ -160,7 +184,7 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	convertedUserId, err := strconv.Atoi(userId)
+	convertedUserID, err := strconv.Atoi(userID)
 	if err != nil {
 		resp := badResponse{Error: err.Error()}
 		jsonResp, err := json.Marshal(resp)
@@ -175,9 +199,9 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	serverEventStorage.addEvent(newEvent(convertedDate, convertedUserId))
+	serverEventStorage.addEvent(newEvent(convertedDate, convertedUserID))
 
-	if !hasDate || !hasUserId {
+	if !hasDate || !hasUserID {
 		resp := badResponse{Error: "no event data was sent"}
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
@@ -202,6 +226,7 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// updateEvent - хэндлер функция для сервера, обновляющая событие
 func updateEvent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -222,8 +247,8 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 
 	hasDate := r.Form.Has("date")
 	date := r.Form.Get("date")
-	hasUserId := r.Form.Has("user_id")
-	userId := r.Form.Get("user_id")
+	hasUserID := r.Form.Has("user_id")
+	userID := r.Form.Get("user_id")
 	hasNewDate := r.Form.Has("user_id")
 	newDate := r.Form.Get("user_id")
 
@@ -257,7 +282,7 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	convertedUserId, err := strconv.Atoi(userId)
+	convertedUserID, err := strconv.Atoi(userID)
 	if err != nil {
 		resp := badResponse{Error: err.Error()}
 		jsonResp, err := json.Marshal(resp)
@@ -272,7 +297,7 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = serverEventStorage.updateEvent(convertedDate, convertedUserId, convertedNewDate)
+	err = serverEventStorage.updateEvent(convertedDate, convertedUserID, convertedNewDate)
 	if err != nil {
 		resp := badResponse{Error: err.Error()}
 		jsonResp, err := json.Marshal(resp)
@@ -287,7 +312,7 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !hasDate || !hasUserId || !hasNewDate {
+	if !hasDate || !hasUserID || !hasNewDate {
 		resp := badResponse{Error: "no event data was sent"}
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
@@ -312,6 +337,7 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// deleteEvent - хэндлер функция для сервера, удаляющая событие
 func deleteEvent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -332,8 +358,8 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 
 	hasDate := r.Form.Has("date")
 	date := r.Form.Get("date")
-	hasUserId := r.Form.Has("user_id")
-	userId := r.Form.Get("user_id")
+	hasUserID := r.Form.Has("user_id")
+	userID := r.Form.Get("user_id")
 
 	convertedDate, err := time.Parse(time.DateOnly, date)
 	if err != nil {
@@ -350,7 +376,7 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	convertedUserId, err := strconv.Atoi(userId)
+	convertedUserID, err := strconv.Atoi(userID)
 	if err != nil {
 		resp := badResponse{Error: err.Error()}
 		jsonResp, err := json.Marshal(resp)
@@ -365,7 +391,7 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = serverEventStorage.removeEvent(convertedDate, convertedUserId)
+	err = serverEventStorage.removeEvent(convertedDate, convertedUserID)
 	if err != nil {
 		resp := badResponse{Error: err.Error()}
 		jsonResp, err := json.Marshal(resp)
@@ -380,7 +406,7 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !hasDate || !hasUserId {
+	if !hasDate || !hasUserID {
 		resp := badResponse{Error: "no event data was sent"}
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
@@ -405,6 +431,7 @@ func deleteEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// getEventsForDay - хэндлер функция для сервера, ищущая событие по дню
 func getEventsForDay(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -470,6 +497,7 @@ func getEventsForDay(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// getEventsForWeek - хэндлер функция для сервера, ищущая событие по неделе
 func getEventsForWeek(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -535,6 +563,7 @@ func getEventsForWeek(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// getEventsForMonth - хэндлер функция для сервера, ищущая событие по месяцу
 func getEventsForMonth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -602,6 +631,7 @@ func getEventsForMonth(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// создание сервера о объявление путей с привязкой хэндлер функций
 	mux := http.NewServeMux()
 	mux.HandleFunc("/create_event", createEvent)
 	mux.HandleFunc("/update_event", updateEvent)
@@ -610,9 +640,10 @@ func main() {
 	mux.HandleFunc("/events_for_week", getEventsForWeek)
 	mux.HandleFunc("/events_for_month", getEventsForMonth)
 
+	// создание контекста
 	ctx := context.Background()
 	server := &http.Server{
-		Addr:    ":3333",
+		Addr:    ":4040",
 		Handler: mux,
 		BaseContext: func(l net.Listener) context.Context {
 			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
@@ -620,6 +651,7 @@ func main() {
 		},
 	}
 
+	// запуск сервера
 	err := server.ListenAndServe()
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
